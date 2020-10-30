960f86d4a908cc73219f769a940fbdc13dde3675
Nathan Schrader <nathanschrader@icloud.com>
2020-10-30 13:54:27 -0700

    add correctly functioning business logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,62 @@ $(document).ready(function()  {
     event.preventDefault();
 
     let userNum = parseInt($("#inputBox").val());
-    let numArray = [];
-    let finalArray = [];
+    let userArray = [];
+    let finalString = [];
+    let userString = "";
 
     for (let i = 0; i <= userNum; i+=1)  {
-      numArray.push(i);
+      userArray.push(i);
     }
 
-    finalArray = numArray.map(function(element)  {
+    userString = userArray.map(function(e){return e.toString()});
+
+    finalString = userString.map(function(element)  {
+      if (element === "0")  {
+        return element;
+      }
+      else if (element.includes("3")) {
+        return "Won't you be my neighbor?";
+      }
+      else if (element.includes("2")) {
+        return "Boop!";
+      }
+      else if (element.includes("1")) {
+        return "Beep!";
+      }
+      else  {
+        return element;
+      }
+    });
+    
+    alert("user Array")
+    console.log(userArray);
+    alert("user String")
+    console.log(userString);
+    alert("final string")
+    console.log(finalString);
+
+  });
+});
+
+
+
+/*
+// working script to replace "1" "2" and "3" //
+
+$(document).ready(function()  {
+  $("form#mrForm").submit(function(event) {
+    event.preventDefault();
+
+    let userNum = parseInt($("#inputBox").val());
+    let userArray = [];
+    let finalString = [];
+
+    for (let i = 0; i <= userNum; i+=1)  {
+      userArray.push(i);
+    }
+
+    finalString = userArray.map(function(element)  {
       if (element === 0)  {
         return element;
       }
@@ -28,32 +76,67 @@ $(document).ready(function()  {
       }
     });
     
-    console.log(numArray);
-    console.log(finalArray);
+    console.log(userArray);
+    console.log(finalString);
 
   });
 });
 
-
-
+*/
 
 
 /*
+$(document).ready(function()  {
+  $("form#mrForm").submit(function(event) {
+    event.preventDefault();
+
+    let userNum = parseInt($("#inputBox").val());
+    let userArray = [];
+    let finalString = userArray.split("");
+    let newArray = [];
+
+    for (let i = 0; i <= userNum; i+=1)  {
+      userArray.push(i);
+    };
+
+    let userString = userArray.map(function(e){return e.toString()});
+    
+
+    finalString.forEach(function(number) {
+      if (userArray.includes(number)) [
+        newArray.push("")
+      ]
+    })
+
+    
+
+
+    finalString = userArray.map(function(element)  {
       if (element === 0)  {
-        element = 5;
+        return element;
       }
-      else {
-      finalArray[element] = element;
+      else if (element === 1) {
+        return "Beep!";
       }
-      
+      else if (element === 2) {
+        return "Boop!";
+      }
+      else if (element === 3) {
+        return "Won't you be my neighbor?";
+      }
+      else  {
+        return element;
+      }
+    });
+    
+    alert("user Array");
+    console.log(userArray);
+    alert("final array")
+    console.log(finalString);
+    alert("new array");
+    console.log(newArray)
 
-        else if (element === 1) {
-          numArray.push("Beep!")
-        }
+  });
+});
 
-
-
-for (let i = 0; i <= userNum; i+=1)  {
-  numArray.push(i);
-};
 */
