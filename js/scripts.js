769c8a8993479fe256a450899d782a5cf49edb25
Nathan Schrader <nathanschrader@icloud.com>
2020-10-30 12:56:46 -0700

    add some functionality to if statement

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,57 @@ $(document).ready(function()  {
     event.preventDefault();
 
     let userNum = parseInt($("#inputBox").val());
-    let userNum2 = userNum;
     let numArray = [];
+    let finalArray = [];
 
     for (let i = 0; i <= userNum; i+=1)  {
       numArray.push(i);
-    };
+    }
+
+    finalArray = numArray.map(function(element)  {
+      if (element === 0)  {
+        return element;
+      }
+      else if (element === 1) {
+        return "Beep!";
+      }
+      else if (element === 2) {
+        return "Boop!";
+      }
+      else if (element === 3) {
+        return "Won't you be my neighbor?";
+      }
+      else  {
+        return element;
+      }
+    });
     
-    console.log(userNum);
     console.log(numArray);
+    console.log(finalArray);
 
   });
 });
+
+
+
+
+
+/*
+      if (element === 0)  {
+        element = 5;
+      }
+      else {
+      finalArray[element] = element;
+      }
+      
+
+        else if (element === 1) {
+          numArray.push("Beep!")
+        }
+
+
+
+for (let i = 0; i <= userNum; i+=1)  {
+  numArray.push(i);
+};
+*/
