3f470fb65b096018a6b47c91316968af96c2ec7c
Nathan Schrader <nathanschrader@icloud.com>
2020-10-30 11:00:19 -0700

    add functionality to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@ $(document).ready(function()  {
   $("form#mrForm").submit(function(event) {
     event.preventDefault();
 
+    let userNum = parseInt($("#inputBox").val());
+    let userNum2 = userNum;
+    let numArray = [];
+
+    for (let i = 0; i <= userNum; i+=1)  {
+      numArray.push(i);
+    };
     
-  })
-})
+    console.log(userNum);
+    console.log(numArray);
+
+  });
+});
