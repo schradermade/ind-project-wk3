0d1aa67310bfe22a3ac1da69be2afd520d1b38ec
Nathan Schrader <nathanschrader@icloud.com>
2020-11-09 13:00:29 -0800

    separate business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,43 @@
+// Business Logic
+let userArray = [];
+let userString = "";
+let finalString = [];
+
+function roboger(userNum) {
+  for (let i = 0; i <= userNum; i+=1)  {
+    userArray.push(i);
+  }
+
+  userString = userArray.map(function(e){return e.toString()});
+  console.log(userString);
+  finalString = userString.map(function(element)  {
+    if (element === "0")  {
+      return element;
+    }
+    else if (element.includes("3")) {
+      return " \"Won't you be my neighbor?\"";
+    }
+    else if (element.includes("2")) {
+      return " \"Boop!\"";
+    }
+    else if (element.includes("1")) {
+      return " \"Beep!\"";
+    }
+    else  {
+      return " " + element;
+    }
+  return finalString;
+  });
+} 
+// User Interface Logic
 $(document).ready(function()  {
   $("form#mrForm").submit(function(event) {
     event.preventDefault();
 
     let userNum = parseInt($("#inputBox").val());
-    let userArray = [];
-    let finalString = [];
-    let userString = "";
-
-    for (let i = 0; i <= userNum; i+=1)  {
-      userArray.push(i);
-    }
-
-    userString = userArray.map(function(e){return e.toString()});
-
-    finalString = userString.map(function(element)  {
-      if (element === "0")  {
-        return element;
-      }
-      else if (element.includes("3")) {
-        return " \"Won't you be my neighbor?\"";
-      }
-      else if (element.includes("2")) {
-        return " \"Boop!\"";
-      }
-      else if (element.includes("1")) {
-        return " \"Beep!\"";
-      }
-      else  {
-        return " " + element;
-      }
-    return finalString;
-    });
+ 
+    roboger(userNum);
     document.getElementById("showString").innerHTML = finalString;
+    
   });
 });
